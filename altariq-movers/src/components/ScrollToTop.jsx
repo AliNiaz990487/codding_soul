c991ae80fa2dbb54718650d980dd99c19a391dca
AliNiaz990487 <alik990487@gmail.com>
2024-09-01 22:02:09 +0500

    fixed z-index

--- altariq-movers/src/components/ScrollToTop.jsx
+++ altariq-movers/src/components/ScrollToTop.jsx
@@ -3,7 +3,7 @@ import { FaArrowUp } from "react-icons/fa6";
 
 const ScrollToTop = () => {
   return (
-    <div className="fixed bottom-10 right-5 px-4 p-2 bg-primary text-white rounded-bl-xl cursor-pointer transition-colors hover:bg-secondary" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+    <div className="z-50 fixed bottom-10 right-5 px-4 p-2 bg-primary text-white rounded-bl-xl cursor-pointer transition-colors hover:bg-secondary" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
       <FaArrowUp />
     </div>
   )
